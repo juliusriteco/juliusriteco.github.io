a6b6dd582ed7097e8d5826bd40b08e7888bd8921
juliusriteco <62749351+juliusriteco@users.noreply.github.com>
2024-10-05 20:28:21 +0200

    new

--- commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AuctionListing, Category, Bidding, User
-# Register your models here.
-
-admin.site.register(AuctionListing)
-admin.site.register(Category)
-admin.site.register(Bidding)
-admin.site.register(User)
